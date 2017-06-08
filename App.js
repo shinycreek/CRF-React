01f9264148c44cc489bde5f31e5ff21dccdbdb7b
vijay chouhan <vijay@gemsessence.com>
2017-06-08 22:25:07 +0530

    added redux and redux thunk

--- App.js
+++ App.js
@@ -1,37 +1,12 @@
-// import React, { Component } from 'react';
-//
-// import {
-//   View,
-//   WebView,
-//   StyleSheet
-// } from 'react-native';
-//
-//
-// export default class App extends Component {
-//   render() {
-//     return (
-//       <View style={{flex: 1}}>
-//           <WebView source={{uri: 'http://catawba.maps.arcgis.com/apps/webappviewer/index.html?id=86a805d54d5546d2b99a73d60b16a19d'}} style={{marginTop: 30, flex: 1}}  />
-//       </View>
-//     );
-//   }
-// }
-//
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
+import React from 'react';
+import { Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import store from './src/configureStore';
+
+const App = () => (
+  <Provider store={store}>
+    <View><Text>Good test</Text></View>
+  </Provider>
+);
+
+export default App;
