ee5de1ae57f5f64464f9011a67d3f60a27d3c2e7
Haiz Oppenheimer <haizop@gmail.com>
2017-08-22 16:08:40 -0400

    fix facebook and reducer

--- src/reducers/followUsPage.js
+++ src/reducers/followUsPage.js
@@ -2,17 +2,17 @@ import Immutable from 'immutable';
 import * as types from '../constants/types';
 
 const initialState = Immutable.fromJS(
-  { url: null, submitting: false },
+  { url: null, loaded: false },
 );
 
 const followUsPage = (state = initialState, action) => {
   switch (action.type) {
     case types.START_FETCH_FOLLOW_US_PAGE_URL:
-      return state.merge({ url: null, submitting: true });
+      return state.merge({ url: null, loaded: false });
     case types.SUCCESS_FETCH_FOLLOW_US_PAGE_URL:
-      return state.merge({ url: action.url, submitting: false });
+      return state.merge({ url: action.url, loaded: true });
     case types.ERROR_FETCH_FOLLOW_US_PAGE_URL:
-      return state.merge({ url: null, submitting: false });
+      return state.merge({ url: null, loaded: false });
     default:
       return state;
   }
